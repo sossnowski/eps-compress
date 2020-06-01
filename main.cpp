1f430ab828cfa6ec0b2f5834b6a5ed6d5673d12a
Krzysztof Brzoska <kbrzoska97@gmail.com>
2020-06-01 11:14:03 +0200

    user interface deleted

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
 #include "File.h"
-#include "UserInterface.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
-    UserInterface u = UserInterface();
-    u.displayInterface();
     //Trigger action button and get user file path
     string filePath = "../epsFiles/przyklad.eps";
     int width = 1000;
-    //u.handleFile(filePath);
     File f = File(filePath);
     f.processData(width);
 
